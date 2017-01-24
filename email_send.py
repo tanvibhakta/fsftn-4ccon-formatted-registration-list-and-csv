7c407864bd333418d67fe56c68b2a9e07586668e
Padawan <tanvibhakta@gmail.com>
2017-01-24 18:37:16 +0530

    uses queries everywhere

--- email_send.py
+++ email_send.py
@@ -3,18 +3,53 @@ from email.mime.text import MIMEText
 
 fromaddr = '4ccon@fsftn.org'
 
+#python talking to the database
 c = sqlite3.connect('db.sqlite3').cursor()
-c.execute('SELECT email FROM tickets_tickets;')
-toaddr = c.fetchone()
 
-fp = open('text.txt', 'rb')
-msg = "Hey, "
-fp.close()
+#setting up the server
+#s = smtplib.SMTP('localhost')
 
-msg['Subject'] = 'Your 4ccon workshop registration'
-msg['From'] = fromaddr
-msg['To'] = toaddr
+c.execute('SELECT * FROM tickets_tickets;')
+field = c.fetchall()
 
-s = smtplib.SMTP('localhost')
-s.sendmail(fromaddr, toaddr, msg.as_string())
-s.quit()
+for i in range(len(field)):
+
+    test = 'tanvibhakta@gmail.com'
+    print "Inside for"
+
+        c.execute('SELECT email FROM tickets_tickets;')
+        toaddr = c.fetchall()
+        print toaddr[i]
+
+        c.execute('SELECT name FROM tickets_tickets;')
+        name = c.fetchall()
+        print name[i]
+
+        c.execute('SELECT order_type FROM tickets_tickets;')
+        order_type = c.fetchall()
+        print order_type[i]
+
+        msg = ("Hey %s,/n/n/r" % (name, ))
+
+        if (order_type is "bronze" or "silver" or "platinum"):
+            c.execute('SELECT title FROM proposals_proposal JOIN tickets_tickets ON proposals_proposal.id == tickets_tickets.workshop_id;')
+            workshop_name = c.fetchall()
+            print workshop_name[i]
+
+            c.execute('SELECT prerequisites FROM proposals_proposal JOIN tickets_tickets ON proposals_proposal.id == tickets_tickets.workshop_id;')
+            prerequisites = c.fetchall()
+
+        if (order_type is "gold" or "platinum"):
+            pass
+            #just text here
+
+
+        msg['Subject'] = 'Your 4ccon workshop registration'
+        msg['From'] = fromaddr
+        msg['To'] = test
+
+
+        #s.sendmail(fromaddr, toaddr, msg.as_string())
+
+
+#s.quit()
