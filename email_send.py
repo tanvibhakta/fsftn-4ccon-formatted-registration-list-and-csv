ab58def0804297e22fbf6285e79ede954fde1ccc
Padawan <tanvibhakta@gmail.com>
2017-01-24 21:38:04 +0530

    completes logic of the email

--- email_send.py
+++ email_send.py
@@ -1,5 +1,6 @@
 import sqlite3, smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 fromaddr = '4ccon@fsftn.org'
 
@@ -9,45 +10,44 @@ c = sqlite3.connect('db.sqlite3').cursor()
 #setting up the server
 #s = smtplib.SMTP('localhost')
 
-c.execute('SELECT * FROM tickets_tickets;')
+c.execute('SELECT * FROM tickets_tickets WHERE status==3 OR status==4;')
 field = c.fetchall()
 
 for i in range(len(field)):
 
     test = 'tanvibhakta@gmail.com'
-    print "Inside for"
 
-        c.execute('SELECT email FROM tickets_tickets;')
-        toaddr = c.fetchall()
-        print toaddr[i]
+    toaddr = field[i][9]
+    print toaddr
 
-        c.execute('SELECT name FROM tickets_tickets;')
-        name = c.fetchall()
-        print name[i]
+    name = field[i][8]
+    print name
 
-        c.execute('SELECT order_type FROM tickets_tickets;')
-        order_type = c.fetchall()
-        print order_type[i]
+    order_type = field[i][6]
+    print order_type
 
-        msg = ("Hey %s,/n/n/r" % (name, ))
+    ticket_id = field[i][0]
+    workshop_id = field[i][17]
 
-        if (order_type is "bronze" or "silver" or "platinum"):
-            c.execute('SELECT title FROM proposals_proposal JOIN tickets_tickets ON proposals_proposal.id == tickets_tickets.workshop_id;')
-            workshop_name = c.fetchall()
-            print workshop_name[i]
+    msg = "Hey, "
+    msg.attach(name)
 
-            c.execute('SELECT prerequisites FROM proposals_proposal JOIN tickets_tickets ON proposals_proposal.id == tickets_tickets.workshop_id;')
-            prerequisites = c.fetchall()
+    if (order_type == "bronze" or order_type == "silver" or order_type == "platinum"):
+        c.execute('SELECT title FROM proposals_proposal JOIN tickets_tickets ON proposals_proposal.id ==?;', (workshop_id, ))
+        workshop_name = c.fetchall()
 
-        if (order_type is "gold" or "platinum"):
-            pass
-            #just text here
+        c.execute('SELECT prerequisites FROM proposals_proposal JOIN tickets_tickets ON proposals_proposal.id == ?;', (workshop_id, ))
+        prerequisites = c.fetchall()
+
+    if (order_type is "gold" or "platinum"):
+        pass
+        #just text here
 
 
-        msg['Subject'] = 'Your 4ccon workshop registration'
-        msg['From'] = fromaddr
-        msg['To'] = test
-
+    # msg['Subject'] = 'Your 4ccon workshop registration'
+    # msg['From'] = fromaddr
+    # msg['To'] = test
+    #
 
         #s.sendmail(fromaddr, toaddr, msg.as_string())
 
